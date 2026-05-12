220c332f200337e9af4cf4b80f0ebf4bd8f27422
David <david.cec@proton.me>
2026-05-13 01:10:37 +0200

    Take into account settings that need restart

--- src/renderer/store/modules/settings.js
+++ src/renderer/store/modules/settings.js
@@ -122,7 +122,7 @@ import { getSystemLocale, showToast } from '../../helpers/utils'
  * and to ensure that the implementation works as intended.
  *
  ***
- * `nonExportableSettings`
+ * `nonExportableKeys`
  * This array contains setting keys
  * that should not be exported when a user chooses to "Export settings".
  *
@@ -424,7 +424,7 @@ const sideEffectHandlers = {
 
 const settingsWithSideEffects = Object.keys(sideEffectHandlers)
 
-const nonExportableSettings = [
+const nonExportableKeys = new Set([
   // Proxy
   'proxyHostname',
   'proxyPort',
@@ -439,15 +439,25 @@ const nonExportableSettings = [
   // Screenshot
   'screenshotAskPath',
   'screenshotFolderPath'
-]
+])
+
+const restartNeededKeys = new Set([
+  'disableSmoothScrolling'
+])
 
 const customState = {
 }
 
 const customGetters = {
   getExportableSettings: (state) => {
-    const exportableSettings = Object.keys(state).filter(key => !nonExportableSettings.includes(key))
+    const exportableSettings = Object.fromEntries(
+      Object.entries(state).filter(([key]) => !nonExportableKeys.has(key))
+    )
     return exportableSettings
+  },
+
+  getRestartNeededKeys: () => {
+    return restartNeededKeys
   }
 }
 
@@ -666,6 +676,10 @@ const customActions = {
 
   getDefaultUpdaterId: (_, settingId) => {
     return defaultUpdaterId(settingId)
+  },
+
+  getDefaultGetterId: (_, settingId) => {
+    return defaultGetterId(settingId)
   }
 }
 
