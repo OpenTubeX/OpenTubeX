24909e384bfddabec11e73de102e79ca3f1d1e97
Nico <nico@d3sox.me>
2026-08-02 17:21:29 +0200

    feat(about): link nightly build commit (#468)
    Resolves #464

--- _scripts/webpack.web.config.js
+++ _scripts/webpack.web.config.js
@@ -135,6 +135,7 @@ const config = {
       'process.env.IS_ELECTRON': false,
       'process.env.IS_ELECTRON_MAIN': false,
       'process.env.SUPPORTS_LOCAL_API': false,
+      'process.env.BUILD_COMMIT': JSON.stringify(process.env.GITHUB_SHA ?? ''),
       __VUE_OPTIONS_API__: 'true',
       __VUE_PROD_DEVTOOLS__: 'false',
       __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: 'false',
