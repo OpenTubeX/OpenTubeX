0ae35b34c7e31db953e6ac3a78b6997171d2ad5b
D3SOX <nico@d3sox.me>
2026-09-19 20:32:33 +0200

    fix(watch): prevent empty Up Next handoff

--- src/renderer/components/FtListVideoLazy.vue
+++ src/renderer/components/FtListVideoLazy.vue
@@ -38,6 +38,7 @@ import { computed, ref } from 'vue'
 import FtListVideo from './FtListVideo/FtListVideo.vue'
 
 import store from '../store/index'
+import { isVideoHiddenByPreferences } from '../helpers/subscriptions'
 
 const props = defineProps({
   data: {
@@ -114,9 +115,6 @@ const props = defineProps({
   }
 })
 
-const visible = ref(props.initialVisibleState)
-const display = ref('block')
-
 const EMPTY_SET = new Set()
 
 /** @type {import('vue').ComputedRef<Set<string>>} */
@@ -138,14 +136,15 @@ const hideChannelsBasedOnText = computed(() => {
 })
 
 const shouldBeVisible = computed(() => {
-  const lowerCaseTitle = props.data.title?.toLowerCase()
-  const lowerCaseAuthor = props.data.author?.toLowerCase()
-
-  return !(channelsHiddenNames.value.has(props.data.authorId) ||
-    channelsHiddenNames.value.has(props.data.author) ||
-    (lowerCaseTitle && forbiddenTitles.value.some((text) => lowerCaseTitle.includes(text))) ||
-    (hideChannelsBasedOnText.value && lowerCaseAuthor && forbiddenTitles.value.some((text) => lowerCaseAuthor.includes(text))))
+  return !isVideoHiddenByPreferences(props.data, {
+    hiddenChannelNames: channelsHiddenNames.value,
+    forbiddenTitles: forbiddenTitles.value,
+    hideChannelsBasedOnText: hideChannelsBasedOnText.value,
+  })
 })
+
+const visible = ref(props.initialVisibleState && shouldBeVisible.value)
+const display = ref(shouldBeVisible.value ? 'block' : 'none')
 
 /**
  * @param {boolean} isVisible
